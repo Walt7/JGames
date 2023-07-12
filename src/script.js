eb8e709e007b264a304a9fd1bb1162a5d704a142
Walter <roxwal@gmail.com>
2023-07-12 20:48:00 +0200

    Pre sprite min 8.08

--- src/script.js
+++ src/script.js
@@ -2,3 +2,14 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
 const can = { w: canvas.width = 600, h: canvas.height = 600 };
+
+const playerImage=new Image();
+playerImage.src ='img/shadow_dog.png';
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(100,50,100,100);
+    ctx.drawImage(playerImage, 50, 0);
+    requestAnimationFrame(animate);
+}
+animate();;
