ffe0ec6d74a2d760403f5309bf5ca646b4be4f3e
Walter <roxwal@gmail.com>
2023-07-13 18:43:17 +0200

    tolto x2 min 1:19:00

--- 02-background/script.js
+++ 02-background/script.js
@@ -12,7 +12,6 @@ class Layer {
         this.y = 0;
         this.width = 2400;
         this.height = 700;
-        this.x2 = this.width;
         this.image = image;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
@@ -25,19 +24,12 @@ class Layer {
     update() {
         this.speed = gameSpeed * this.speedModifier;
         if (this.x < -this.width)
-            this.x = this.width + this.x2 - this.speed;
-        else
-            this.x -= gameSpeed;
-        if (this.x2 < -this.width)
-            this.x2 = this.width + this.x - this.speed;
-        else
-            this.x2 -= gameSpeed;
+            this.x = 0;
         this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
     }
     draw() {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
     }
 }
 
