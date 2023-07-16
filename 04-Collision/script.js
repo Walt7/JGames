4eb0409c6d1b6f4233d9ee65bce9ba2e9e21f564
Walter <roxwal@gmail.com>
2023-07-16 12:30:23 +0200

    canvas rotate min 2:46:53

--- 04-Collision/script.js
+++ 04-Collision/script.js
@@ -11,12 +11,13 @@ class esplosione {
     constructor(pos) {
         this.sprite = { W: 200, H: 179 };
         this.sz = { W: this.sprite.W / 2, H: this.sprite.H / 2 };
-        this.x = pos[0] - this.sz.W / 2;
-        this.y = pos[1] - this.sz.H / 2;
+        this.x = pos[0];
+        this.y = pos[1];
         this.image = new Image();
         this.image.src = 'img/boom.png';
         this.frame = 0;
         this.flapStep = 3;
+        this.angle = Math.random() * 6.2;
     }
     finito() {
         return this.frame > 6
@@ -33,11 +34,16 @@ class esplosione {
         }
     }
     draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
         //ctx.strokeRect(this.x, this.y, this.width, this.height)
-        ctx.drawImage(this.image, this.frame * this.sprite.W, 0, ...toArr(this.sprite), this.x, this.y, ...toArr(this.sz))
+        ctx.drawImage(this.image, this.frame * this.sprite.W, 0,
+            ...toArr(this.sprite), 0 - this.sz.W / 2, 0 - this.sz.H / 2, ...toArr(this.sz))
+        ctx.restore();
     }
 }
-const  createAnimation =e=> esplosioni.push(new esplosione([e.x - can.pos.left, e.y - can.pos.top]))
+const createAnimation = e => esplosioni.push(new esplosione([e.x - can.pos.left, e.y - can.pos.top]))
 
 window.addEventListener('click', createAnimation);
 
