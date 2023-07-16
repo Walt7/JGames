399e08c6d55d6ab5a945cba7050005ff6824e763
Walter <roxwal@gmail.com>
2023-07-16 11:34:45 +0200

    Esplosione Min 2:41.53

--- 04-Collision/script.js
+++ 04-Collision/script.js
@@ -8,54 +8,49 @@ const toArr = o => [o.W, o.H];
 
 const esplosioni = [];
 class esplosione {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+    constructor(pos) {
         this.sprite = { W: 200, H: 179 };
         this.sz = { W: this.sprite.W / 2, H: this.sprite.H / 2 };
+        this.x = pos[0] - this.sz.W / 2;
+        this.y = pos[1] - this.sz.H / 2;
         this.image = new Image();
         this.image.src = 'img/boom.png';
         this.frame = 0;
+        this.flapStep = 5;
     }
     run() {
+        if (this.frame > 6) return // esplosione completata
         this.update();
         this.draw();
     }
     update() {
 
         if (!(GameFrame % (this.flapStep * 5))) {
-            this.frame = ++this.frame % 5;
-            this.angle += this.angleSpeed;
+            this.frame++;
         }
     }
     draw() {
         //ctx.strokeRect(this.x, this.y, this.width, this.height)
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, ...toArr(this.sprite), this.x, this.y, ...toArr(this.sz))
+        ctx.drawImage(this.image, this.frame * this.sprite.W, 0, ...toArr(this.sprite), this.x, this.y, ...toArr(this.sz))
     }
 }
 
 window.addEventListener('click', e => {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x - can.pos.left, e.y - can.pos.top, 50, 50);
+    const pos = [e.x - can.pos.left, e.y - can.pos.top];
+    esplosioni.push(new esplosione(pos))
 })
-
-
 
 let GameFrame = 0;
 
 
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    esplosioni.forEach(l => l.run());
+    GameFrame = new Date().valueOf()
+    //GameFrame++;
+    requestAnimationFrame(animate);
+}
 
-
-/*
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        nemici.forEach(l => l.run());
-        GameFrame = new Date().valueOf()
-        //GameFrame++;
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-*/
+animate();
 
 
